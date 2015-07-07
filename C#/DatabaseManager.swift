569de58733ccee57afa0ccb8eb650b297db6ee01
kevinfsilva <kevin.ferreira.silva@gmail.com>
2015-07-07 10:43:40 -0300

    Atualizei o DatabaseManager

--- C#/DatabaseManager.swift
+++ C#/DatabaseManager.swift
@@ -14,19 +14,19 @@ class DatabaseManager
 
 lazy var managedObjectModel: NSManagedObjectModel = {
     // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-    let modelURL = NSBundle.mainBundle().URLForResource("C_", withExtension: "momd")!
+    let modelURL = NSBundle.mainBundle().URLForResource("C#", withExtension: "momd")!
     return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
 
-lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-    let url = urls[urls.count-1].URLByAppendingPathComponent("C_.sqlite")
+lazy var managedObjectContext: NSManagedObjectContext? = {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
     var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+    let directory = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+    let url = directory[directory.count-1].URLByAppendingPathComponent("C#.sqlite")
     var error: NSError? = nil
     var failureReason = "There was an error creating or loading the application's saved data."
     if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
         coordinator = nil
-        // Report any error we got.
         var dict = [String: AnyObject]()
         dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
         dict[NSLocalizedFailureReasonErrorKey] = failureReason
@@ -37,13 +37,6 @@ lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
         NSLog("Unresolved error \(error), \(error!.userInfo)")
         abort()
     }
-    
-    return coordinator
-    }()
-
-lazy var managedObjectContext: NSManagedObjectContext? = {
-    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-    let coordinator = self.persistentStoreCoordinator
     if coordinator == nil {
         return nil
     }
@@ -51,8 +44,6 @@ lazy var managedObjectContext: NSManagedObjectContext? = {
     managedObjectContext.persistentStoreCoordinator = coordinator
     return managedObjectContext
     }()
-
-// MARK: - Core Data Saving support
 
 func saveContext () {
     if let moc = self.managedObjectContext {
