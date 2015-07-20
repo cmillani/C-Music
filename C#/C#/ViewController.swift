cc58c541b4c35ce2ef5c37948a4f2916f9477356
kevinfsilva <kevin.ferreira.silva@gmail.com>
2015-07-20 14:45:14 -0300

    Arrumei a animacao

--- C#/C#/ViewController.swift
+++ C#/C#/ViewController.swift
@@ -39,36 +39,28 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
         self.view.addGestureRecognizer(swipeRight)
         self.view.addGestureRecognizer(swipeLeft)
         
-//        self.path1.moveToPoint(CGPoint(x: 360, y: 870))
         self.path1.addCurveToPoint(CGPoint(x: 235, y: 845), controlPoint1: CGPoint(x: 320, y: 865), controlPoint2: CGPoint(x: 270, y: 855))
         
-//        self.path2.moveToPoint(CGPoint(x: 235, y: 845))
         self.path2.addCurveToPoint(CGPoint(x: 380, y: 820), controlPoint1: CGPoint(x: 270, y: 825), controlPoint2: CGPoint(x: 320, y: 835))
         
-//        self.path3.moveToPoint(CGPoint(x: 380, y: 820))
         self.path3.addCurveToPoint(CGPoint(x: 485, y: 845), controlPoint1: CGPoint(x: 410, y: 825), controlPoint2: CGPoint(x: 440, y: 835))
         
-//        self.path4.moveToPoint(CGPoint(x: 485, y: 845))
         self.path4.addCurveToPoint(CGPoint(x: 360, y: 870), controlPoint1: CGPoint(x: 400, y: 850), controlPoint2: CGPoint(x: 440, y: 860))
         
-//        self.path5.moveToPoint(CGPoint(x: 360, y: 870))
         self.path5.addCurveToPoint(CGPoint(x: 485, y: 845), controlPoint1: CGPoint(x: 400, y: 860), controlPoint2: CGPoint(x: 440, y: 850))
         
-//        self.path6.moveToPoint(CGPoint(x: 485, y: 845))
         self.path6.addCurveToPoint(CGPoint(x: 380, y: 820), controlPoint1: CGPoint(x: 410, y: 835), controlPoint2: CGPoint(x: 440, y: 825))
         
-//        self.path7.moveToPoint(CGPoint(x: 380, y: 820))
         self.path7.addCurveToPoint(CGPoint(x: 235, y: 845), controlPoint1: CGPoint(x: 340, y: 825), controlPoint2: CGPoint(x: 300, y: 830))
         
-//        self.path8.moveToPoint(CGPoint(x: 235, y: 845))
         self.path8.addCurveToPoint(CGPoint(x: 360, y: 870), controlPoint1: CGPoint(x: 270, y: 855), controlPoint2: CGPoint(x: 325, y: 862))
         
     }
     
     @IBAction func OpenExistent(sender: AnyObject) {
-//        var screen = RecordViewController()
+        //        var screen = RecordViewController()
         self.performSegueWithIdentifier("firstLoader", sender: self)
-
+        
         
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -93,8 +85,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
     {
         if(self.positionControl == 1)
         {
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
                 self.newImage.transform = CGAffineTransformMakeScale(0.6, 0.6)
                 let anim = CAKeyframeAnimation(keyPath: "position")
                 anim.path = self.path1.CGPath
@@ -112,27 +103,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
                 anim3.repeatCount = 0
                 self.colectionImage.center = CGPoint(x: 540, y: 905)
                 self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
-                })
-            
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
-                self.newImage.transform = CGAffineTransformMakeScale(0.3, 0.3)
-                let anim2 = CAKeyframeAnimation(keyPath: "position")
-                anim2.path = self.path2.CGPath
-                anim2.duration = 1.0
-                anim2.rotationMode = kCAAnimationLinear
-                anim2.repeatCount = 0
-                self.newImage.center = CGPoint(x: 404, y: 850)
-                self.newImage.layer.addAnimation(anim2, forKey: "animate position along path")
-                
-                self.colectionImage.transform = CGAffineTransformMakeScale(2, 2)
-                let anim3 = CAKeyframeAnimation(keyPath: "position")
-                anim3.path = self.path4.CGPath
-                anim3.duration = 1.0
-                anim3.rotationMode = kCAAnimationLinear
-                anim3.repeatCount = 0
-                self.colectionImage.center = CGPoint(x: 400, y: 920)
-                self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                }, completion: { (res) -> Void in
+                    
+                    UIView.animateWithDuration(0.7, animations: { () -> Void in
+                        self.newImage.transform = CGAffineTransformMakeScale(0.3, 0.3)
+                        let anim2 = CAKeyframeAnimation(keyPath: "position")
+                        anim2.path = self.path2.CGPath
+                        anim2.duration = 1.0
+                        anim2.rotationMode = kCAAnimationLinear
+                        anim2.repeatCount = 0
+                        self.newImage.center = CGPoint(x: 404, y: 850)
+                        self.newImage.layer.addAnimation(anim2, forKey: "animate position along path")
+                        
+                        self.colectionImage.transform = CGAffineTransformMakeScale(2, 2)
+                        let anim3 = CAKeyframeAnimation(keyPath: "position")
+                        anim3.path = self.path4.CGPath
+                        anim3.duration = 1.0
+                        anim3.rotationMode = kCAAnimationLinear
+                        anim3.repeatCount = 0
+                        self.colectionImage.center = CGPoint(x: 400, y: 920)
+                        self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                    })
             })
             self.positionControl = 2
             self.collectionButton.hidden = false
@@ -143,8 +134,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
             self.positionControl = 1
             self.collectionButton.hidden = true
             self.newButton.hidden = false
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
                 self.colectionImage.transform = CGAffineTransformMakeScale(1.4, 1.4)
                 let anim = CAKeyframeAnimation(keyPath: "position")
                 anim.path = self.path1.CGPath
@@ -162,27 +152,29 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
                 anim3.repeatCount = 0
                 self.newImage.center = CGPoint(x: 540, y: 905)
                 self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                }, completion: { (res) -> Void in
+                    UIView.animateWithDuration(0.7, animations: { () -> Void in
+                        
+                        self.colectionImage.transform = CGAffineTransformMakeScale(1, 1)
+                        let anim2 = CAKeyframeAnimation(keyPath: "position")
+                        anim2.path = self.path2.CGPath
+                        anim2.duration = 1.0
+                        anim2.rotationMode = kCAAnimationLinear
+                        anim2.repeatCount = 0
+                        self.colectionImage.center = CGPoint(x: 404, y: 850)
+                        self.colectionImage.layer.addAnimation(anim2, forKey: "animate position along path")
+                        
+                        self.newImage.transform = CGAffineTransformMakeScale(1, 1)
+                        let anim3 = CAKeyframeAnimation(keyPath: "position")
+                        anim3.path = self.path4.CGPath
+                        anim3.duration = 1.0
+                        anim3.rotationMode = kCAAnimationLinear
+                        anim3.repeatCount = 0
+                        self.newImage.center = CGPoint(x: 400, y: 920)
+                        self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                    })
             })
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
-                self.colectionImage.transform = CGAffineTransformMakeScale(1, 1)
-                let anim2 = CAKeyframeAnimation(keyPath: "position")
-                anim2.path = self.path2.CGPath
-                anim2.duration = 1.0
-                anim2.rotationMode = kCAAnimationLinear
-                anim2.repeatCount = 0
-                self.colectionImage.center = CGPoint(x: 404, y: 850)
-                self.colectionImage.layer.addAnimation(anim2, forKey: "animate position along path")
-                
-                self.newImage.transform = CGAffineTransformMakeScale(1, 1)
-                let anim3 = CAKeyframeAnimation(keyPath: "position")
-                anim3.path = self.path4.CGPath
-                anim3.duration = 1.0
-                anim3.rotationMode = kCAAnimationLinear
-                anim3.repeatCount = 0
-                self.newImage.center = CGPoint(x: 400, y: 920)
-                self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
-            })
+            
         }
     }
     
@@ -190,8 +182,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
     {
         if(self.positionControl == 1)
         {
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
                 self.newImage.transform = CGAffineTransformMakeScale(0.6, 0.6)
                 let anim = CAKeyframeAnimation(keyPath: "position")
                 anim.path = self.path5.CGPath
@@ -209,27 +200,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
                 anim3.repeatCount = 0
                 self.colectionImage.center = CGPoint(x: 283, y: 905)
                 self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
-            })
-            
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
-                self.newImage.transform = CGAffineTransformMakeScale(0.3, 0.3)
-                let anim2 = CAKeyframeAnimation(keyPath: "position")
-                anim2.path = self.path6.CGPath
-                anim2.duration = 1.0
-                anim2.rotationMode = kCAAnimationLinear
-                anim2.repeatCount = 0
-                self.newImage.center = CGPoint(x: 404, y: 850)
-                self.newImage.layer.addAnimation(anim2, forKey: "animate position along path")
-                
-                self.colectionImage.transform = CGAffineTransformMakeScale(2, 2)
-                let anim3 = CAKeyframeAnimation(keyPath: "position")
-                anim3.path = self.path8.CGPath
-                anim3.duration = 1.0
-                anim3.rotationMode = kCAAnimationLinear
-                anim3.repeatCount = 0
-                self.colectionImage.center = CGPoint(x: 400, y: 920)
-                self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                }, completion: { (res) -> Void in
+                    UIView.animateWithDuration(0.7, animations: { () -> Void in
+                        
+                        self.newImage.transform = CGAffineTransformMakeScale(0.3, 0.3)
+                        let anim2 = CAKeyframeAnimation(keyPath: "position")
+                        anim2.path = self.path6.CGPath
+                        anim2.duration = 1.0
+                        anim2.rotationMode = kCAAnimationLinear
+                        anim2.repeatCount = 0
+                        self.newImage.center = CGPoint(x: 404, y: 850)
+                        self.newImage.layer.addAnimation(anim2, forKey: "animate position along path")
+                        
+                        self.colectionImage.transform = CGAffineTransformMakeScale(2, 2)
+                        let anim3 = CAKeyframeAnimation(keyPath: "position")
+                        anim3.path = self.path8.CGPath
+                        anim3.duration = 1.0
+                        anim3.rotationMode = kCAAnimationLinear
+                        anim3.repeatCount = 0
+                        self.colectionImage.center = CGPoint(x: 400, y: 920)
+                        self.colectionImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                    })
             })
             self.positionControl = 2
             self.collectionButton.hidden = false
@@ -240,8 +231,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
             self.positionControl = 1
             self.collectionButton.hidden = true
             self.newButton.hidden = false
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
+            UIView.animateWithDuration(0.7, animations: { () -> Void in
                 self.colectionImage.transform = CGAffineTransformMakeScale(1.4, 1.4)
                 let anim = CAKeyframeAnimation(keyPath: "position")
                 anim.path = self.path5.CGPath
@@ -259,26 +249,28 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
                 anim3.repeatCount = 0
                 self.newImage.center = CGPoint(x: 283, y: 905)
                 self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
-            })
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                
-                self.colectionImage.transform = CGAffineTransformMakeScale(1, 1)
-                let anim2 = CAKeyframeAnimation(keyPath: "position")
-                anim2.path = self.path6.CGPath
-                anim2.duration = 1.0
-                anim2.rotationMode = kCAAnimationLinear
-                anim2.repeatCount = 0
-                self.colectionImage.center = CGPoint(x: 404, y: 850)
-                self.colectionImage.layer.addAnimation(anim2, forKey: "animate position along path")
-                
-                self.newImage.transform = CGAffineTransformMakeScale(1, 1)
-                let anim3 = CAKeyframeAnimation(keyPath: "position")
-                anim3.path = self.path8.CGPath
-                anim3.duration = 1.0
-                anim3.rotationMode = kCAAnimationLinear
-                anim3.repeatCount = 0
-                self.newImage.center = CGPoint(x: 400, y: 920)
-                self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                }, completion: { (res) -> Void in
+                    UIView.animateWithDuration(1.0, animations: { () -> Void in
+                        
+                        self.colectionImage.transform = CGAffineTransformMakeScale(1, 1)
+                        let anim2 = CAKeyframeAnimation(keyPath: "position")
+                        anim2.path = self.path6.CGPath
+                        anim2.duration = 1.0
+                        anim2.rotationMode = kCAAnimationLinear
+                        anim2.repeatCount = 0
+                        self.colectionImage.center = CGPoint(x: 404, y: 850)
+                        self.colectionImage.layer.addAnimation(anim2, forKey: "animate position along path")
+                        
+                        self.newImage.transform = CGAffineTransformMakeScale(1, 1)
+                        let anim3 = CAKeyframeAnimation(keyPath: "position")
+                        anim3.path = self.path8.CGPath
+                        anim3.duration = 1.0
+                        anim3.rotationMode = kCAAnimationLinear
+                        anim3.repeatCount = 0
+                        self.newImage.center = CGPoint(x: 400, y: 920)
+                        self.newImage.layer.addAnimation(anim3, forKey: "animate position along path")
+                    })
+                    
             })
         }
     }
@@ -297,4 +289,3 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, LoadViewCon
         newImage.center = CGPoint(x: self.view.center.x, y: self.view.center.y - 70)
     }
 }
-
